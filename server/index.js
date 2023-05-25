d9920adf83515f178c768cc003db4f3fe650943b
ABHISHEK VERMA <104015898+AbhiiiDev@users.noreply.github.com>
2023-05-25 19:10:48 +0530

    demo data added manually from index.js

--- server/index.js
+++ server/index.js
@@ -10,8 +10,13 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import authRoutes from './routes/auth.js'
 import {register} from "./controllers/auth.js"
+import {createPost} from './controllers/posts.js'
 import { verifyToken } from './middleware/auth.js'
 import userRoutes from './routes/users.js'
+import postRoutes from './routes/posts.js'
+import User from './models/User.js';
+import Post from './models/Post.js';
+import {users,posts} from './data/index.js';
 
 
 //configurations
@@ -47,11 +52,13 @@ const upload =multer({storage})
 
 //ROUTES WITH FILES
 app.post("/auth/register",upload.single("picture"),verifyToken , register);
+app.post("/posts",verifyToken,upload.single('picture'),createPost);         
 //securing this post routes
 
 //ROUTES 
  app.use("/auth",authRoutes);
  app.use("/user",userRoutes);
+ app.use('/posts',postRoutes);
 
 //MONGOOSE SETUP
 
@@ -62,5 +69,9 @@ mongoose.connect(process.env.MONGO_URL,{
     
 }). then(()=>{
     app.listen(PORT,()=> console.log(`server listening at port ${PORT}`))
+
+    //Adding demo data to mongodb only one time
+    // User.insertMany(users);
+    // Post.insertMany(posts);
 })
 .catch((error)=>console.log(`${error} did not connect`));
