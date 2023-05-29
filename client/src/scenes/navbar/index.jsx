0f7f11b49a0373c8909dce19d328558d090b3da4
ABHISHEK VERMA <104015898+AbhiiiDev@users.noreply.github.com>
2023-05-29 23:47:39 +0530

    navbar inititated (with error)

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import {
     Box,
     IconButton,
@@ -11,7 +11,7 @@ import {
     useMediaQuery
 } from '@mui/material';
 import {
-    Search, 
+    Search,
     Message,
     DarkMode,
     LightMode,
@@ -21,32 +21,50 @@ import {
     Close
 } from '@mui/icons-material';
 
-import { useDispatch,useSelector } from 'react-redux';
-import { setMode,setLogout } from 'state';
-import {useNavigate} from 'react-router-dom'
-import FlexBetween from 'components/FlexBetween';
+import { useDispatch, useSelector } from 'react-redux';
+import { setMode, setLogout } from 'state';
+import { useNavigate } from 'react-router-dom'
+import FlexBetween from '/src/components/FlexBetween.jsx';
 
 
 
-const Navbar = ()=> {
+const Navbar = () => {
 
-    const [isMobileMenuToggled,setIsMobileMenuToggled]=useState(false);//to on and off mobile menu on small screen
- const dispatch = useDispatch();
- const navigate=useNavigate();
- const user= useSelector((state)=>state.user);
- const isNonMobileScreens=useMediaQuery("(min-width:1000px)");
+    const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);//to on and off mobile menu on small screen
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const user = useSelector((state) => state.user);
+    const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
 
-const theme =useTheme();
-const neutalLight=theme.palette.neutral.light;
-const dark=theme.palette.neutral.dark;
-const background=theme.palette.background.default;
-const primaryLight=theme.palette.primary.light;
-const alt=theme.palette.background.alt;
+    const theme = useTheme();
+    const neutalLight = theme.palette.neutral.light;
+    const dark = theme.palette.neutral.dark;
+    const background = theme.palette.background.default;
+    const primaryLight = theme.palette.primary.light;
+    const alt = theme.palette.background.alt;
 
-const fullName = `${user.firstName} ${user.lastName}` ;
+    const fullName = `${user.firstName} ${user.lastName}`;
 
-return <FlexBetween padding="1rem 6%" backgroundColor={alt}></FlexBetween>
+    return <FlexBetween padding="1rem 6%" backgroundColor={alt}>
+        <FlexBetween gap='1.7rem'>
+<Typography
+fontWeight="bold"
+fontSize='clamp(1rem,2rem,2.5rem)'
+color='primary'
+onClick={()=>navigate('/home')}
+sx={{
+    "&:hover":{
+        color:primaryLight,
+        cursor:"pointer",
 
-    
+    },
+}}
+>
+SocialNetwork
+</Typography>
+        </FlexBetween>
+    </FlexBetween>
+
+
 };
- export default Navbar;
+export default Navbar;
